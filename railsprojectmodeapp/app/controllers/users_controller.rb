65598709f72b51da17b5a5c0b6d86b0a396dea23
KellyStamps <kellydsample@gmail.com>
2018-01-04 19:25:51 -0500

    added mod join table, mod abilities, still working on bugs

--- railsprojectmodeapp/app/controllers/users_controller.rb
+++ railsprojectmodeapp/app/controllers/users_controller.rb
@@ -58,7 +58,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation, :email, :birth_date)
+    params.require(:user).permit(:username, :password, :password_confirmation, :email, :birth_date, :admin, :moderator)
   end
 
 end
--- railsprojectmodeapp/app/models/category.rb
+++ railsprojectmodeapp/app/models/category.rb
@@ -2,7 +2,8 @@ class Category < ApplicationRecord
   has_many :posts
   has_many :user_categories
   has_many :users, through: :user_categories
-
+  has_many :mod_categories
+  has_many :categories, through: :mod_categories
 
 
 
